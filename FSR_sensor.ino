f41005b2db1c85aa95ebc4bdef151ced7ba93468
Shota Inoue <0my32347242141b@ezweb.ne.jp>
2018-01-19 11:55:10 +0900

    Update FSR_sensor.ino

--- FSR_sensor.ino
+++ FSR_sensor.ino
@@ -6,9 +6,12 @@ void setup() {
 void loop() {
   delay(1000);
   int a = analogRead(A0);
-  double Va,Ra,Rb = 10*10^3,V = 5;
-  Va = a * V / 1023.0;
-  Ra = (Rb / V - Va) * Va;
+  double Va,Vb,Ra,Rb = 1*10^3,V = 5,i;
+  Vb = a * V / 1023.0;
+  Va = V -Vb;
+  i = V/Rb;
+  Ra = Va/i;
+  //Ra = (Rb / V - Va) * Va;
   Serial.println(String(Va));
   Serial.println(String(Ra));
 }
